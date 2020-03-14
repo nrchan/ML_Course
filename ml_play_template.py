292923c29e4d80c34630cf0541e03110dbdb0168
nrchan <luka.iwt@gmail.com>
2020-03-14 11:49:18 +0800

    fixed some stupid and fatal error

--- ml_play_template.py
+++ ml_play_template.py
@@ -45,19 +45,25 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-        esti_ball_x = calculate(prev_ball_coor[0], prev_ball_coor[1], scene_info.ball[0], scene_info.ball[1], scene_info.platform[0], scene_info.platform[1])
+        esti_ball_x = calculate(prev_ball_coor[0], prev_ball_coor[1], scene_info.ball[0], scene_info.ball[1], prev_plat_coor[0], scene_info.platform[0])
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            if esti_ball_x > scene_info.platform[0]:
+            if esti_ball_x > (scene_info.platform[0] + 20):
+                print("Estimate: ",esti_ball_x,", Current: ",scene_info.platform[0] + 20,", Moving right")
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-            elif esti_ball_x < scene_info.platform[0]:
+            elif esti_ball_x < (scene_info.platform[0] + 20):
                 comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                print("Estimate: ",esti_ball_x,", Current: ",scene_info.platform[0] + 20,", Moving left")
             else:
                 comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+                print("Estimate: ",esti_ball_x,", Current: ",scene_info.platform[0] + 20,", Staying")
+        
+        prev_ball_coor = (scene_info.ball[0], scene_info.ball[1])
+        prev_plat_coor = (scene_info.platform[0], scene_info.platform[1])
 
 # calculate the possible x posotion when ball is at y=400
 def calculate(prev_ball_x, prev_ball_y, cur_ball_x, cur_ball_y, prev_plat, cur_plat):
@@ -68,7 +74,7 @@ def calculate(prev_ball_x, prev_ball_y, cur_ball_x, cur_ball_y, prev_plat, cur_p
 
     if prev_ball_y > cur_ball_y:
         # use > for the coordination is up side down
-        return cur_ball_x -2
+        return cur_ball_x
     else:
         try:
             m = (cur_ball_y - prev_ball_y)/(cur_ball_x - prev_ball_x)
@@ -79,15 +85,15 @@ def calculate(prev_ball_x, prev_ball_y, cur_ball_x, cur_ball_y, prev_plat, cur_p
         # x        = (y - y0)/m + x0
         candidate = (400 - cur_ball_y)/(m if m != 0 else 1) + cur_ball_x -2
         if candidate >= 0 and candidate <= 400:
-            return candidate -2
+            return candidate
         elif candidate > 400:
             if (candidate/400) % 2 == 1:
-                return 400 - candidate % 400 -2
+                return 400 - candidate % 400
             else:
-                return candidate % 400 -2
+                return candidate % 400
         else:
             candidate = abs(candidate)
             if (candidate/400) % 2 == 0:
-                return candidate % 400 -2
+                return candidate % 400
             else:
-                return 400 - candidate % 400 -2
+                return 400 - candidate % 400
